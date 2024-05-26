e6b175ebd7429c0ac623748d56a5d03e4183bc59
Ttunay <tigrus232323k@gmail.com>
2024-05-26 19:14:42 +0300

    platformio for ESP8266

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,49 +1,8 @@
-#include <WiFi.h>
-
-const char* ssid     = "agaga ";
-const char* password = "tak ya i pokazal ";
-
-WiFiServer server(80);
-
 void setup()
 {
-    Serial.begin(115200);
-
-    delay(100);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-    }
-
-    server.begin();
 }
 
-void loop(){
-    WiFiClient client = server.available();
+void loop()
+{
 
-    if (client) {
-        String currentLine = "";
-        while (client.connected()) {
-            if (client.available()) {
-                char c = client.read();
-                if (c == '\n') {
-                    if (currentLine.length() == 0) {
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-type:text/html");
-                        client.println();
-                        client.print("EPS32 HELLLOOssO");
-                        client.println();
-                        break;
-                    } else {
-                        currentLine = "";
-                    }
-                } else if (c != '\r') {
-                    currentLine += c;
-                }
-            }
-        }
-        client.stop();
-    }
 }
